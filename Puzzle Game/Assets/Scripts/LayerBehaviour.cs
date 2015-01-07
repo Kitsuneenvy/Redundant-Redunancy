f06759b9f8acb3bb57d7a0dc1c108efaf524b7ec
KitsuneEnvy <lachlangodthelp@hotmail.com>
2015-01-07 19:07:00 +1100

    Puzzle Game commit

---
+++ Puzzle Game/Assets/Scripts/LayerBehaviour.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[System.Serializable]
+public class Layer {
+	public int ID;
+	public float correctAngle;
+	public float currentAngle;
+	public Color currentColor;
+	public Color defaultColor;
+
+}
+
+public class LayerBehaviour : MonoBehaviour {
+
+	public Layer LayerInfo;
+	
+	Vector2 touchDeltaPosition;
+	public bool ActiveLayer = false;
+	Color originalColor;
+	bool correctAngle = false;
+	// Use this for initialization
+	void Start () {
+		originalColor = this.GetComponent<SpriteRenderer>().color;
+		LayerInfo.defaultColor = originalColor;
+		LayerInfo.currentColor = originalColor;
+		LayerInfo.currentAngle = this.transform.rotation.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(ActiveLayer == true){
+			if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved){
+				
+				touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+				float touchDeltaX = Input.GetTouch(0).deltaPosition.x;
+				float touchDeltaY = Input.GetTouch(0).deltaPosition.y;
+				if(Mathf.Abs(touchDeltaX) > Mathf.Abs(touchDeltaY)){
+					if(Mathf.Abs(touchDeltaX) > 0){
+						transform.Rotate(Vector3.forward,touchDeltaX*-4);
+						LayerInfo.currentAngle = this.transform.rotation.z;
+//					} else {
+//						transform.Rotate(Vector3.forward,touchDeltaX*4);
+//						LayerInfo.currentAngle = this.transform.rotation.z;
+//					}
+					}
+					if(this.transform.rotation.z <= LayerInfo.correctAngle +4 && this.transform.rotation.z >= LayerInfo.correctAngle -4){
+						correctAngle = true;
+						//this.GetComponent<SpriteRenderer>().color = Color.blue;
+					}
+				} else if (Mathf.Abs(touchDeltaX) < Mathf.Abs(touchDeltaY)){
+					this.GetComponent<SpriteRenderer>().color = Color.blue;
+					if(touchDeltaY > 0){
+						GameObject.FindGameObjectWithTag("GameController").GetComponent<Manager>().switchLayerUp();
+					} else {
+						GameObject.FindGameObjectWithTag("GameController").GetComponent<Manager>().switchLayerDown();
+					}
+				}
+			}
+		}
+	}
+
+	void OnGUI (){
+		if(ActiveLayer){
+			GUI.Label(new Rect(Screen.width/5, 10, 100, 20), this.name);
+		}
+	}
+
+
+
+
+	public void setActiveLayer(bool newValue){
+		ActiveLayer = newValue;
+		if(ActiveLayer == false){
+			LayerInfo.currentColor = new Color(LayerInfo.currentColor.r,LayerInfo.currentColor.g,LayerInfo.currentColor.b,100);
+		} else {
+			LayerInfo.currentColor = LayerInfo.defaultColor;
+		}
+		this.GetComponent<SpriteRenderer>().color = LayerInfo.currentColor;
+	}
+}
---
+++ Puzzle Game/Assets/Scripts/Manager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Manager : MonoBehaviour {
+
+	Vector2 touchDeltaPosition;
+	public GameObject currentLayer;
+	int currentLayerID;
+	public GameObject currentWorld;
+	List<SpriteRenderer> layerRenderers = new List<SpriteRenderer>();
+	List<GameObject> layers = new List<GameObject>();
+	int level = 0;
+	int timesCalled = 0;
+	string nameOfFoundLayer;
+
+
+	// Use this for initialization
+	void Start () {
+
+		foreach(SpriteRenderer layerRenderer in currentWorld.transform.GetComponentsInChildren<SpriteRenderer>()){
+			layerRenderers.Add(layerRenderer);
+		}
+		for(int i = 0; i<currentWorld.transform.childCount; i++){
+			layers.Add(currentWorld.transform.GetChild(i).gameObject);
+		}
+		currentLayerID = currentLayer.GetComponent<LayerBehaviour>().LayerInfo.ID;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touchCount>0){
+			touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+		}
+
+	}
+
+	void OnGUI(){
+		GUI.Label(new Rect(10, 10, 100, 20), currentLayerID.ToString());
+		GUI.Label(new Rect(Screen.width/2, 10, 100, 20), touchDeltaPosition.ToString());
+		GUI.Label(new Rect(Screen.width/4, 10, 100, 20), nameOfFoundLayer);
+	}
+
+	void switchLayer(GameObject newLayer){
+		currentLayer.GetComponent<LayerBehaviour>().setActiveLayer(false);
+		currentLayer = newLayer;
+		newLayer.GetComponent<LayerBehaviour>().setActiveLayer(true);
+		currentLayerID = currentLayer.GetComponent<LayerBehaviour>().LayerInfo.ID;
+
+	}
+
+	GameObject FindLayerByID(int IDToFind){
+		foreach(GameObject layer in layers){
+			if(layer.GetComponent<LayerBehaviour>().LayerInfo.ID == IDToFind){
+				return layer;
+			}
+		}
+		return null;
+	}
+	
+	public void switchLayerUp(){
+		if(currentLayerID!=layers.Count){
+			switchLayer(FindLayerByID(currentLayerID+1));
+			nameOfFoundLayer = FindLayerByID(currentLayerID+1).name;
+		}
+
+	}
+	public void switchLayerDown(){
+		if(currentLayerID!=0){
+			switchLayer(FindLayerByID(currentLayerID-1));
+			nameOfFoundLayer = FindLayerByID(currentLayerID+1).name;
+		}
+	}
+}
