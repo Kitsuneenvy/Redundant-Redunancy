b4ed5c729405d30e317c6bda7b11e288e2a10069
KitsuneEnvy <lachlangodthelp@hotmail.com>
2015-01-21 21:42:18 +1100

    A lot of new things and stuff.

--- Puzzle Game/Assets/Scripts/LayerBehaviour.cs
+++ Puzzle Game/Assets/Scripts/LayerBehaviour.cs
@@ -5,7 +5,6 @@ using System.Collections;
 [System.Serializable]
 public class Layer {
 	public int ID;
-	public float correctAngle;
 	public float currentAngle;
 	public Color currentColor;
 	public Color defaultColor;
@@ -19,14 +18,19 @@ public class LayerBehaviour : MonoBehaviour {
 	Vector2 touchDeltaPosition;
 	public bool ActiveLayer = false;
 	Color originalColor;
-	bool correctAngle = false;
+	public bool correctAngle = false;
+	bool changedLayer = false;
+	bool movingLayer = false;
+	bool aboveCorrect = false;
+	bool belowCorrect = false;
 	// Use this for initialization
 	void Start () {
 		originalColor = this.GetComponent<SpriteRenderer>().color;
 		LayerInfo.defaultColor = originalColor;
 		LayerInfo.currentColor = originalColor;
 		LayerInfo.currentAngle = this.transform.rotation.z;
-
+		this.GetComponent<SpriteRenderer>().color = LayerInfo.currentColor;
+		this.transform.eulerAngles= new Vector3(0,0,Random.Range(0.0f,360.0f));
 	}
 	
 	// Update is called once per frame
@@ -39,45 +43,80 @@ public class LayerBehaviour : MonoBehaviour {
 				float touchDeltaY = Input.GetTouch(0).deltaPosition.y;
 				if(Mathf.Abs(touchDeltaX) > Mathf.Abs(touchDeltaY)){
 					if(Mathf.Abs(touchDeltaX) > 0){
-						transform.Rotate(Vector3.forward,touchDeltaX*-4);
+						transform.Rotate(Vector3.forward,touchDeltaX*-2);
 						LayerInfo.currentAngle = this.transform.rotation.z;
-//					} else {
-//						transform.Rotate(Vector3.forward,touchDeltaX*4);
-//						LayerInfo.currentAngle = this.transform.rotation.z;
-//					}
 					}
-					if(this.transform.rotation.z <= LayerInfo.correctAngle +4 && this.transform.rotation.z >= LayerInfo.correctAngle -4){
+					LayerInfo.currentAngle = clampRotation(this.transform.rotation.z);
+					checkAboveAndBelow();
+					if(aboveCorrect==true && belowCorrect ==true){
 						correctAngle = true;
-						//this.GetComponent<SpriteRenderer>().color = Color.blue;
 					}
-				} else if (Mathf.Abs(touchDeltaX) < Mathf.Abs(touchDeltaY)){
-					this.GetComponent<SpriteRenderer>().color = Color.blue;
+//					if(LayerInfo.currentAngle <= 4 || LayerInfo.currentAngle >= 356){
+//						correctAngle = true;
+//						this.GetComponent<SpriteRenderer>().color = Color.blue;
+//					} else {
+//						this.GetComponent<SpriteRenderer>().color = LayerInfo.defaultColor;
+//					}
+					movingLayer = true;
+				} else if ((Mathf.Abs(touchDeltaX) < Mathf.Abs(touchDeltaY))&&changedLayer == false&&movingLayer == false){
 					if(touchDeltaY > 0){
 						GameObject.FindGameObjectWithTag("GameController").GetComponent<Manager>().switchLayerUp();
+						changedLayer = true;
 					} else {
 						GameObject.FindGameObjectWithTag("GameController").GetComponent<Manager>().switchLayerDown();
+						changedLayer = true;
 					}
 				}
+			} else if(Input.touchCount == 0){
+				changedLayer = false;
+				movingLayer = false;
 			}
 		}
 	}
 
 	void OnGUI (){
-		if(ActiveLayer){
-			GUI.Label(new Rect(Screen.width/5, 10, 100, 20), this.name);
-		}
 	}
 
 
-
+	public void setCurrentAlpha(float alpha){
+		LayerInfo.currentColor = new Color(LayerInfo.currentColor.r,LayerInfo.currentColor.g,LayerInfo.currentColor.b,alpha);
+		this.GetComponent<SpriteRenderer>().color = LayerInfo.currentColor;
+	}
 
 	public void setActiveLayer(bool newValue){
 		ActiveLayer = newValue;
 		if(ActiveLayer == false){
-			LayerInfo.currentColor = new Color(LayerInfo.currentColor.r,LayerInfo.currentColor.g,LayerInfo.currentColor.b,100);
+			//LayerInfo.currentColor = new Color(LayerInfo.currentColor.r,LayerInfo.currentColor.g,LayerInfo.currentColor.b,alpha);
 		} else {
 			LayerInfo.currentColor = LayerInfo.defaultColor;
+			this.GetComponent<SpriteRenderer>().color = LayerInfo.currentColor;
 		}
-		this.GetComponent<SpriteRenderer>().color = LayerInfo.currentColor;
+	}
+
+	float clampRotation(float rotation){
+		float newRotation = 0;
+		if(rotation<0){
+			newRotation = rotation+360;
+		} else if( rotation >360){
+			newRotation = rotation -360;
+		}
+		if(newRotation<0||newRotation>360){
+			clampRotation(newRotation);
+			return 0;
+		} else {
+			return newRotation;
+		}
+	}
+	void checkAboveAndBelow(){
+		if(GameObject.FindGameObjectWithTag("GameController").GetComponent<Manager>().returnAboveAndBelowCorrect(LayerInfo.ID).x == 1){
+			aboveCorrect = true;
+		} else {
+			aboveCorrect = false;
+		}
+		if(GameObject.FindGameObjectWithTag("GameController").GetComponent<Manager>().returnAboveAndBelowCorrect(LayerInfo.ID).y == 1){
+			belowCorrect = true;
+		} else {
+			belowCorrect = false;
+		}
 	}
 }
--- Puzzle Game/Assets/Scripts/Manager.cs
+++ Puzzle Game/Assets/Scripts/Manager.cs
@@ -6,18 +6,22 @@ public class Manager : MonoBehaviour {
 
 	Vector2 touchDeltaPosition;
 	public GameObject currentLayer;
-	int currentLayerID;
+	public int currentLayerID;
 	public GameObject currentWorld;
 	List<SpriteRenderer> layerRenderers = new List<SpriteRenderer>();
 	List<GameObject> layers = new List<GameObject>();
 	int level = 0;
 	int timesCalled = 0;
 	string nameOfFoundLayer;
+	GameObject mainCamera = null;
+	public float cameraStartPosition = 3;
+	public float cameraMovementMultiplier = 0.5f;
+	bool initialised = false;
 
 
 	// Use this for initialization
 	void Start () {
-
+		mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
 		foreach(SpriteRenderer layerRenderer in currentWorld.transform.GetComponentsInChildren<SpriteRenderer>()){
 			layerRenderers.Add(layerRenderer);
 		}
@@ -25,10 +29,23 @@ public class Manager : MonoBehaviour {
 			layers.Add(currentWorld.transform.GetChild(i).gameObject);
 		}
 		currentLayerID = currentLayer.GetComponent<LayerBehaviour>().LayerInfo.ID;
+		mainCamera.transform.position = new Vector3(mainCamera.transform.position.x,cameraStartPosition+cameraMovementMultiplier*currentLayerID,mainCamera.transform.position.z);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(!initialised){
+			switchLayer(currentLayer);
+			initialised = true;
+		}
+		if(Input.GetKeyDown(KeyCode.A)){
+			//switchLayer(FindLayerByID(currentLayerID+1));
+			switchLayerUp();
+		}
+		if(Input.GetKeyDown(KeyCode.S)){
+			switchLayerDown();
+		}
 		if(Input.touchCount>0){
 			touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 		}
@@ -37,15 +54,20 @@ public class Manager : MonoBehaviour {
 
 	void OnGUI(){
 		GUI.Label(new Rect(10, 10, 100, 20), currentLayerID.ToString());
-		GUI.Label(new Rect(Screen.width/2, 10, 100, 20), touchDeltaPosition.ToString());
-		GUI.Label(new Rect(Screen.width/4, 10, 100, 20), nameOfFoundLayer);
+		GUI.Label(new Rect(Screen.width/2, 10, 100, 20), layers.Count.ToString());
 	}
 
 	void switchLayer(GameObject newLayer){
 		currentLayer.GetComponent<LayerBehaviour>().setActiveLayer(false);
 		currentLayer = newLayer;
 		newLayer.GetComponent<LayerBehaviour>().setActiveLayer(true);
+		foreach(GameObject layer in layers){
+			if(layer.GetComponent<LayerBehaviour>().LayerInfo.ID!=newLayer.GetComponent<LayerBehaviour>().LayerInfo.ID){
+				layer.GetComponent<LayerBehaviour>().setCurrentAlpha(Mathf.Abs(((1.0f/(float)(((float)layer.GetComponent<LayerBehaviour>().LayerInfo.ID-(float)newLayer.GetComponent<LayerBehaviour>().LayerInfo.ID)/-5.0f))/10.0f)));
+			}
+		}
 		currentLayerID = currentLayer.GetComponent<LayerBehaviour>().LayerInfo.ID;
+		mainCamera.transform.position = new Vector3(mainCamera.transform.position.x,cameraStartPosition+cameraMovementMultiplier*currentLayerID,mainCamera.transform.position.z);
 
 	}
 
@@ -59,16 +81,26 @@ public class Manager : MonoBehaviour {
 	}
 	
 	public void switchLayerUp(){
-		if(currentLayerID!=layers.Count){
+		if(currentLayerID!=layers.Count-1){
 			switchLayer(FindLayerByID(currentLayerID+1));
-			nameOfFoundLayer = FindLayerByID(currentLayerID+1).name;
+			nameOfFoundLayer = FindLayerByID(currentLayerID).name;
 		}
 
 	}
 	public void switchLayerDown(){
 		if(currentLayerID!=0){
 			switchLayer(FindLayerByID(currentLayerID-1));
-			nameOfFoundLayer = FindLayerByID(currentLayerID+1).name;
+			nameOfFoundLayer = FindLayerByID(currentLayerID).name;
 		}
+	}
+	public Vector2 returnAboveAndBelowCorrect(int passedID){
+		Vector2 aboveBelow = Vector2.zero;
+		if(Mathf.Abs(FindLayerByID(passedID).GetComponent<LayerBehaviour>().LayerInfo.currentAngle- FindLayerByID(passedID+1).GetComponent<LayerBehaviour>().LayerInfo.currentAngle)<=4){
+			aboveBelow.x = 1;
+		}
+		if(Mathf.Abs(FindLayerByID(passedID).GetComponent<LayerBehaviour>().LayerInfo.currentAngle- FindLayerByID(passedID-1).GetComponent<LayerBehaviour>().LayerInfo.currentAngle)<=4){
+			aboveBelow.y = 1;
+		}
+		return aboveBelow;
 	}
 }
